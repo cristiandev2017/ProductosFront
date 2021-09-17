257477706d990a3fd543122289219bde774cd074
Cristian Quiñones <camilo.santamaria322@gmail.com>
2021-09-17 12:38:10 -0500

    Se le agrega el eliminar con ventanas dinamicas y se le agrega otro boton al agregar

--- src/pages/Listar.js
+++ src/pages/Listar.js
@@ -1,5 +1,7 @@
 import { React, useState, useEffect } from 'react'
 import clientAxios from '../config/axios';
+import Swal from 'sweetalert2'
+import { useHistory} from 'react-router-dom'
 
 
 const Listar = () => {
@@ -11,12 +13,43 @@ const Listar = () => {
     useEffect(async () => {
         const response = await clientAxios.get('/list');
         setProducts(response.data);
-    }, [])
+    }, [products])
+
+    const history = useHistory();
+
+    const  eliminarProducto = (id,name) =>{
+        console.log("Traigo el id", id);
+        Swal
+        .fire({
+            title: name,
+            text: "¿Con seguridad desea eliminar este producto?",
+            icon: 'error',
+            showCancelButton: true,
+            confirmButtonText: "Sí, eliminar",
+            cancelButtonText: "Cancelar",
+        })
+        .then(resultado => {
+            if (resultado.value) {
+                // Hicieron click en "Sí"
+                console.log("*se elimina la venta*");
+                clientAxios.delete("/eliminar/"+id)
+            } else {
+                // Dijeron que no
+                console.log("*NO se elimina la venta*");
+            }
+        });
+    }
+
+    const guardarProduct = () =>{
+        history.push("/guardar");
+    }
 
 
     return (
         <div className="text-center container">
-            <h4 className="mt-5">Lista de Productos 🥑</h4>
+            <h4 className="mt-5">Lista de Productos 🥑 <button className="btn btn-dark" onClick={() => guardarProduct() }>+</button></h4> 
+            <i className="bi bi-bag-plus-fill"></i>
+            
             <table className="table mt-4 table-responsive">
                 <thead className="table-dark">
                     <tr>
@@ -37,7 +70,7 @@ const Listar = () => {
                                 <td>{element.nombre}</td>
                                 <td>{element.precio}</td>
                                 <td><button className="btn btn-success">Actualizar</button></td>
-                                <td><button className="btn btn-danger">Eliminar</button></td>
+                                <td><button className="btn btn-danger" onClick={() =>eliminarProducto(element.id, element.nombre)}>Eliminar</button></td>
                                 </tr>
                                 </>
                             )   
